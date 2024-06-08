c75ecaa207bdafe38147420ba310c5590ee8cac2
SatanTech <140904777+SatanTech@users.noreply.github.com>
2024-06-08 11:20:33 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,8 @@
-apt --fix-missing update && apt update && apt upgrade -y && apt install -y wget screen
+#!/bin/bash
+echo -e  "${tyblue}┌──────────────────────────────────────────┐${NC}"
+echo -e  "${tyblue}│              MASUKKAN NAMA KAMU          │${NC}"
+echo -e  "${tyblue}└──────────────────────────────────────────┘${NC}"
+echo " "
+until [[ $name =~ ^[a-zA-Z0-9_.-]+$ ]]; do
+read -rp "Masukan Nama Kamu Disini tanpa spasi : " -e name
+done
