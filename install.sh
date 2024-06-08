2a9aef951209d92d65365f7902c9b04f4b0e153d
SatanTech <140904777+SatanTech@users.noreply.github.com>
2024-06-08 11:38:42 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sysctl -w net.ipv6.conf.all.disable_ipv6=1 >/dev/null 2>&1
 sysctl -w net.ipv6.conf.default.disable_ipv6=1 >/dev/null 2>&1
-./tools.sh
+bash tools.sh
 clear
 start=$(date +%s)
 ln -fs /usr/share/zoneinfo/Asia/Jakarta /etc/localtime
